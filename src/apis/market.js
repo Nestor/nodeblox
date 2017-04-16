3e6a58bcd61967c56799d00e1f7e2ce14fd632d1
Winfamy <infamy@unyx.io>
2017-04-15 23:38:39 -0500

    added fetchAssetThumbnail, made it work

--- src/apis/market.js
+++ src/apis/market.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const png = require('pngjs').PNG
 
 var API = {
 	fetchAssetInfo: (asset_id) => {
@@ -12,6 +13,23 @@ var API = {
 				if(json.errors) return reject( new Error("Invalid assetId") )
 
 				resolve(json)
+			})
+		})
+	},
+
+	fetchAssetThumbnail: (asset_id) => {
+		return new Promise((resolve, reject) => {
+			request({
+				url: `https://www.roblox.com/Thumbs/Asset.ashx?width=420&height=420&assetId=${asset_id}`
+			}, function (err, resp, body) {
+				if(err) return reject(err)
+
+				resolve({
+					url: resp.request.uri.href,
+					base64: new Buffer(body).toString('base64')
+				})
+
+				// console.log(string)
 			})
 		})
 	}
